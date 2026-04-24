bc7b07f76837352edd5b7d4ff995692973f3720b
Mainak Dan <mainak.dan@grenoble-inp.fr>
2026-04-24 13:24:31 +0200

    updated model trainer

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from src.dlProject_energy_demand_forcasting.utils.exception import CustomExcepti
 from src.dlProject_energy_demand_forcasting.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from src.dlProject_energy_demand_forcasting.pipeline.stage_02_data_validation import DataValidationPipeline
 from src.dlProject_energy_demand_forcasting.pipeline.stage_03_data_transformation import DataTransformationPipeline
+from src.dlProject_energy_demand_forcasting.pipeline.stage_04_model_trainer import ModelTrainingPipeline
 
 try:
     obj = DataIngestionPipeline()
@@ -21,6 +22,12 @@ except Exception as e:
 
 try:
     obj = DataTransformationPipeline()
+    obj.main()
+except Exception as e:
+    raise CustomException(e, sys)
+
+try:
+    obj = ModelTrainingPipeline()
     obj.main()
 except Exception as e:
     raise CustomException(e, sys)
